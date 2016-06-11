8dcc47d8e3eb54a6538d401b2be0fad8a5143c88
Jon Edwards <jkedwards@me.com>
2016-06-11 20:37:27 +0000

    completing functionality with related users for bids and query magic

--- tbay.py
+++ tbay.py
@@ -17,6 +17,7 @@ class Item(Base):
     start_time = Column(DateTime, default = datetime.utcnow)
     
     owner_id = Column(Integer, ForeignKey('users.id'), nullable = False)
+    bids = relationship("Bid", backref = 'item')
     
 class User(Base):
     __tablename__ = 'users'
@@ -25,8 +26,8 @@ class User(Base):
     username = Column(String, nullable = False)
     password = Column(String, nullable = False)
     
-    items = relationship("Item", uselist = False, backref="owner")
-    
+    items = relationship("Item", uselist = False, backref = "owner")
+    bids = relationship("Bid", backref = 'bidder')
     
 class Bid(Base):
     __tablename__ = 'bids'
@@ -34,13 +35,22 @@ class Bid(Base):
     id = Column(Integer, primary_key = True)
     price = Column(Float, nullable = False)
     
+    item_id = Column(Integer, ForeignKey('items.id'), nullable = False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
+    
 Base.metadata.create_all(engine)
 
-steve = User(username = 'steve', password = 'password')
-george = User(username = 'george', password = 'password')
-bob = User(username = 'bob', password = 'password')
+steve = User(username = 'Steve', password = 'password')
+george = User(username = 'George', password = 'password')
+bob = User(username = 'Bob', password = 'password')
 
 baseball = Item(name = 'baseball', description = 'almost like new in box', owner = steve)
 
-session.add_all([steve, george, bob, baseball])
+bid = Bid(price = 9.99, item = baseball, bidder = george)
+bid2 = Bid(price = 10.99, item = baseball, bidder = bob)
+bid3 = Bid(price = 11.99, item = baseball, bidder = george)
+session.add_all([steve, george, bob, baseball, bid, bid2, bid3])
 session.commit()
+
+winning_bid = session.query(Bid).filter(Item.name == 'baseball').order_by(Bid.price.desc()).first()
+print(winning_bid.bidder.username, 'won the auction for the', winning_bid.item.name, 'with a bid of ${}'.format(winning_bid.price))
